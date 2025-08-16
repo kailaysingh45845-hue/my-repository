0c14d12f0c7b39a568e145e053e17cfbf46fdb36
GitHub Action <action@github.com>
2025-08-16 17:23:55 +0000

    Initialize template repository proper
    Set package name to com.github.kailaysingh45845-hue.myrepository
    Set project name to my-repository
    Set modid to myrepository

--- src/main/java/com/github/kailaysingh45845-hue/myrepository/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/kailaysingh45845-hue/myrepository/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.kailaysingh45845-hue.myrepository.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
